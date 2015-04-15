b3e2b8422403f47122adfbc68bcbeaf3a649d341
Wikberg <snokjonte@hotmail.com>
2015-04-15 13:33:22 +0200

    head
    lagt till gogglefont

--- inc/head.php
+++ inc/head.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
   	<link rel="stylesheet" type="text/css" href="css/style.css">
+  	<link href='http://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
   	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
   	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 
