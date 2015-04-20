2fb07b77ed045b438a19369d6313a24553616063
Kriller <kriller83@hotmail.com>
2015-04-20 13:24:29 +0200

    fix med ovrigt.php och annat kul

--- ovrigt.php
+++ ovrigt.php
@@ -5,7 +5,7 @@
 <div class="container">
 	<div class="content">
 		<div class="row">
-			<div class="col-xs-6 col-md-3">
+			<div class="col-xs-4 col-md-offset-1 col-md-3">
 		    	<div class="thumbnail">
 		      		<img id="ovrigt" src="img/bild.jpg" alt="...">
 			     		<div class="caption">
@@ -13,8 +13,8 @@
 			        		<p>Övriga produkter</p>
 			        		<p><a href="#" class="btn btn-primary" role="button">Läs mer</a> <a href="#" class="btn btn-default" role="button">Köp</a></p>
 			      		</div>
-			 	</div>
-			</div>
+			      	</div>
+			  </div>
 		</div>
 	</div>
 </div>
