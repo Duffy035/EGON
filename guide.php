e6215cf61e33caddd49211af4ebe7b0847b0e06e
Kriller <kriller83@hotmail.com>
2015-04-20 15:04:19 +0200

    fixat guide.php

---
+++ guide.php
@@ -0,0 +1,23 @@
+<?php include'inc/head.php';?>
+<?php include'inc/template.php'; ?>
+<?php include'inc/nav.php'; ?> 
+
+<div class="container">
+	<div class="content">
+		<div class="row">
+			<div class="col-xs-4">
+		    	<img src="img/yellow.jpeg" alt="Gul" class="guideimg">
+		    		<p>Gul färg hohååååå!!</p></div>
+		       	
+
+
+		       	<div class="col-xs-4"><img src="img/yellow.jpeg" alt="Gul" class="guideimg">
+		    	
+		    		<p>Gul färg hohååååå!!</p></div>
+		       	<div class="col-xs-4"><img src="img/yellow.jpeg" alt="Gul" class="guideimg">
+		    		<p>Gul färg hohååååå!!</p>
+		    </div>
+		</div>
+</div>
+</body>
+</html>
