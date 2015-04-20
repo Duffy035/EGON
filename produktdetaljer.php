08fa0e1c8a1e83be11be23b402760f70b16fd6fb
Wikberg <snokjonte@hotmail.com>
2015-04-20 11:16:24 +0200

    detalj
    fixat på detaljsidan

--- produktdetaljer.php
+++ produktdetaljer.php
@@ -1,23 +1,66 @@
-<!DOCTYPE html>
-<html>
-<meta charset="Utf-8">
-<script src="js/jquery-1.11.2.min.js"></script>
-<script src="js/lightbox.min.js"></script>
-<link href="css/lightbox.css" rel="stylesheet"/>
-
-<head>
-</head>
-	<body>
-		<div class="images">
-		<a href="img/img1.png" data-lightbox="Bilder" data-title="Falken dominerar."><img src="img/img1small.jpg"></a>
-		
-		<a href="img/img2.jpg" data-lightbox="Bilder" data-title="Pelle Jönsson"><img src="img/img2small.jpg"></a>
-		
-		<a href="img/img3.jpg" data-lightbox="Bilder" data-title="Vetenskapen påvisar: Ungdomar är sjukt biffiga nu för tiden"><img src="img/img3small.jpg"></a>
-		
-		<a href="img/img4.png" data-lightbox="Bilder" data-title="Ny SM-mästare: Malin Skoghag"><img src="img/img4small.jpg"></a>
-		<!-- för att lägga till nya bilder, gör en ny rad som dessa ovan, byt a href till den stora bilden, och img src till tumnageln-->
-
+<?php include'inc/head.php';?>
+<?php include'inc/template.php'; ?>
+<?php include'inc/nav.php'; ?> 
+<div class="container">
+	<div class="content">
+		<div class="row" id="produktinfo">
+  			<div class="col-xs-4">Produktnamn</div>
+  			<div class="col-xs-6">Pis:-</div>
+			
+			<div class="col-xs-4"> 
+				<div class="images">
+					<a href="img/img1.png" data-lightbox="Bilder" data-title="Falken dominerar."><img src="img/img1small.jpg"></a>
+					<a href="img/img2.jpg" data-lightbox="Bilder" data-title="Pelle Jönsson"><img src="img/img2small.jpg"></a>
+					<a href="img/img3.jpg" data-lightbox="Bilder" data-title="Vetenskapen påvisar: Ungdomar är sjukt biffiga nu för tiden"><img src="img/img3small.jpg"></a>
+					<a href="img/img4.png" data-lightbox="Bilder" data-title="Ny SM-mästare: Malin Skoghag"><img src="img/img4small.jpg"></a>
+						<!-- för att lägga till nya bilder, gör en ny rad som dessa ovan, byt a href till den stora bilden, och img src till tumnageln-->
+				</div>
+			</div><br>
+			<div class="col-xs-2">
+			<p>Färg 1</p>
+				<select class="form-control">
+  					<option>1</option>
+  					<option>2</option>
+  					<option>3</option>
+  					<option>4</option>
+  					<option>5</option>
+				</select>
+			</div><br>
+			<div class="col-xs-2">
+			<p>Färg 2</p>
+				<select class="form-control">
+  					<option>1</option>
+  					<option>2</option>
+  					<option>3</option>
+  					<option>4</option>
+  					<option>5</option>
+				</select>
+			</div><br>
+			<div class="col-xs-2">
+			<p>Storlek cm</p>
+				<select class="form-control">
+  					<option>15</option>
+  					<option>16</option>
+  					<option>17</option>
+  					<option>18</option>
+  					<option>19</option>
+  					<option>20</option>
+  					<option>21</option>
+  					<option>22</option>
+  					<option>23</option>
+				</select>
+			</div><br>
+			<div class="col-xs-2">
+			<p>Spänne</p>
+				<select class="form-control">
+  					<option>Knut</option>
+  					<option>Plast</option>
+  					<option>Metall</option>
+				</select>
+			</div>
 		</div>
-	</body>
+	</div>
+</div>
+
+</body>
 </html>
