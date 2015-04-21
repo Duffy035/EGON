b8680c19fb6321ef31d8afa058a2bcd91303916a
Wikberg <snokjonte@hotmail.com>
2015-04-21 11:26:41 +0200

    Media query
    tar bort sunbnav vid 480px och lägger till en dropdown ist.

--- inc/nav.php
+++ inc/nav.php
@@ -1,3 +1,28 @@
+<style>
+@media screen and (max-width: 480px) {
+    .subnav{
+      display: none;
+    }
+
+     .big{
+      display: none;
+    }
+}
+
+@media screen and (min-width: 480px) {
+    .small{
+      display: none;
+    }
+    .big{
+      display: inline-block;
+    }
+}
+</style>
+
+
+
+
+
 <div class="container">
 <nav class="navbar navbar-default">
   <div class="container-fluid">
@@ -17,8 +42,30 @@
     <div class="nav">
       <ul class="nav navbar-nav">
         <li><a href="index.php">Hem</a></li>
+        <div class="big">
       	<li><a href="armband.php">Armband</a></li>
+        </div>
+        <div class="small">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Armaband<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">5.50 Cord</a></li>
+              <li><a href="#">Type 1 Cord</a></li>
+            </ul>
+          </li>
+        </div>
+        <div class="big"> 
     		<li><a href="hund.php">Hund</a></li>
+        </div>
+        <div class="small">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hund<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Halsband</a></li>
+              <li><a href="#">Koppel</a></li>
+            </ul>
+          </li>
+        </div>  
     		<li><a href="ovrigt.php">Övrigt</a></li>
     		<li><a href="guide.php">Färg & stoleks guide</a></li>
     		<li><a href="kontakt.php">Kontakt</a></li>
