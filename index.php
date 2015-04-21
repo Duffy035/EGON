f7a10ade7f224b8fa2484651195371572696dd21
Kriller <kriller83@hotmail.com>
2015-04-21 11:25:12 +0200

    fontjobb

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<div class="content"> 
 			<div id="welcome">
 				<p> Välkommen till Paracord Fashion !</p><br>
-				<p>Vi är ett veteranägt företagt som tillverkar varje arband för hand. Varje armband är unikt, därav kan leveranstiden variera.<br> Alla som arbetar inom Paracord Fashion har anknytning till Försvarsmakten och Utlandsstyrkan.
+				<p id="textintro">Vi är ett veteranägt företagt som tillverkar varje arband för hand. Varje armband är unikt, därav kan leveranstiden variera.<br> Alla som arbetar inom Paracord Fashion har anknytning till Försvarsmakten och Utlandsstyrkan.
 				 </p>
 			</div>
 		</div>
