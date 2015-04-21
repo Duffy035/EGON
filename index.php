26bf6789b578261fa4a8816eebdbe12a65413a4e
Kriller <kriller83@hotmail.com>
2015-04-21 16:05:20 +0200

    pill...

--- index.php
+++ index.php
@@ -4,11 +4,26 @@
 	<div class="container">
 		<div class="content"> 
 			<div id="welcome">
-				<p> Välkommen till Paracord Fashion!</p><br></div>
+				<p> Välkommen till Paracord Fashion!</p><br></div><br></br>
 				<div id="textintro"><p>Vi är ett veteranägt företagt som tillverkar varje arband för hand. Varje armband är unikt, därav kan leveranstiden variera.<br> Alla som arbetar inom Paracord Fashion har anknytning till Försvarsmakten och Utlandsstyrkan.
 				 </p>
 			</div>
-		</div>
+				<div class="tajt">
+					<div class="row">
+						<div class="col-xs-4">
+					    	<img src="img/yellow.jpeg" alt="Gul" class="guideimg">
+					    	<p>Gul färg</p>
+					    </div>
+					    <div class="col-xs-4">
+					    	<img src="img/grey.jpeg" alt="Grå" class="guideimg">
+					    	<p>Grå färg</p>
+					    </div>
+					    <div class="col-xs-4">
+					    	<img src="img/red.jpeg" alt="Red" class="guideimg">
+					    	<p>Röd färg</p>
+			    </div>	
+			</div>
+			
 	</div>
 </body>
 
