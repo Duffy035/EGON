1d22f083e4f98d0370d5f6318b93d084f7ddec81
Wikberg <snokjonte@hotmail.com>
2015-04-21 09:51:33 +0200

    Fixat style
    Fixat style på guide sidan och lagt till en beskrivning.

--- guide.php
+++ guide.php
@@ -8,29 +8,36 @@
 			<div class="row">
 				<div class="col-xs-4">
 			    	<img src="img/yellow.jpeg" alt="Gul" class="guideimg">
-			    		<p>Gul färg</p></div>
-			       		<div class="col-xs-4"><img src="img/grey.jpeg" alt="Grå" class="guideimg">
-			    		<p>Grå färg</p></div>
-			       		<div class="col-xs-4"><img src="img/red.jpeg" alt="Red" class="guideimg">
-			    		<p>Röd färg</p>
+			    	<p>Gul färg</p>
 			    </div>
+			    <div class="col-xs-4">
+			    	<img src="img/grey.jpeg" alt="Grå" class="guideimg">
+			    	<p>Grå färg</p>
+			    </div>
+			    <div class="col-xs-4">
+			    	<img src="img/red.jpeg" alt="Red" class="guideimg">
+			    	<p>Röd färg</p>
+			    </div>	
 			</div>
-		</div>
-	</div>
-		<div class="container">
-			<div class="content">
-				<div class="row">
-						<div class="tajt">
-							<div class="col-xs-4">
-						    	<img src="img/blue.jpeg" alt="Blå" class="guideimg">
-						    		<p>Blå färg</p></div>
-						       		<div class="col-xs-4"><img src="img/black.jpeg" alt="Svart" class="guideimg">
-						    		<p>Svart färg</p></div>
-						    	</div>	
-						    </div>
-				  		</div>
-					</div>
+			<div class="row">
+				<div class="col-xs-4">
+					<img src="img/blue.jpeg" alt="Blå" class="guideimg">
+			    	<p>Blå färg</p>
+			   	</div>
+		       	<div class="col-xs-4">
+		       		<img src="img/black.jpeg" alt="Svart" class="guideimg">
+			   		<p>Svart färg</p>
+				 </div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-xs-12">
+					<p>För att Du skall få ett armband som passar perfekt så skall du göra följande: Mät med ett måttband tight runt den handled du vill ha ditt armband. Sedan väljer du det måttet i rullistan när du beställer. Vi kommer att lägga till den mängd så att ditt armband sitter perfekt.</p>
 				</div>
-			<!--För att Du skall få ett armband som passar perfekt så skall du göra följande: Mät med ett måttband tight runt den handled du vill ha ditt armband. Sedan väljer du det måttet i rullistan när du beställer. Vi kommer att lägga till den mängd så att ditt armband sitter perfekt. -->
-	</body>
+			</div>
+		</div>	
+	</div>
+</div>
+			
+</body>
 </html>
