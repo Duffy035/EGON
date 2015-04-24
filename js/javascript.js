df91373705bd749c5528374e254ad1d1d4b735f9
Duffy035 <hejjacarl@hotmail.com>
2015-04-24 15:19:52 +0200

    glömde denna

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+		$(document).ready(function(){
+ 		 var val = $('.val').val();
+		 alert(val);
+	});
+
+
+		$(document).ready(function(){
+ 		 var stl = $('.val').prop('stl');
+		 alert(stl);
+	});
