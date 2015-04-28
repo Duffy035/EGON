953d6c98b4912a04b866dd5212e28dcbc4ee2fdb
Kriller <kriller83@hotmail.com>
2015-04-28 12:30:56 +0200

    massa prylar front-end

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<div id="footer" class="navbar">
+				<button onclick="myFunction()">Läs mer</button>
+				<script>
+				function myFunction() {
+				alert("Läs mer");
+				}
+				</script>
+
+				|<a>Det här är Slinkans hemsida, deal with it. Copyright 2015</a></div>
--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 	<div class="container">
 		<div class="content"> 
 			<div id="welcome">
-				<p> Välkommen till Paracord Fashion!</p><br></div><br></br>
+				<p> Välkommen till Paracord Fashion!</p><br></div>
 				<div id="textintro"><p>Vi är ett veteranägt företagt som tillverkar varje arband för hand. Varje armband är unikt, därav kan leveranstiden variera.<br> Alla som arbetar inom Paracord Fashion har anknytning till Försvarsmakten och Utlandsstyrkan.
-				 </p>
+				 </p><br></br>
 			</div>
 				<div class="tajt">
 					<div class="row">
@@ -20,11 +20,14 @@
 					    </div>
 					    <div class="col-xs-4">
 					    	<img src="img/blaa.png" alt="Red" class="guideimg">
-					    	<p>Blå</p>
+					    	<p>Blå</p><br></br><br></br>
 			    </div>	
 			</div>
-			
+			   
+			</div>
+			 <?php include 'inc/footer.php'; ?>
 	</div>
+
 </body>
 
 </html>
