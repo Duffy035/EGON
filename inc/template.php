bcb182ff89cc3928e3f7bdc64dd2a53e2563b929
Duffy035 <hejjacarl@hotmail.com>
2015-04-28 09:36:13 +0200

    för live test
    wiho!

--- inc/template.php
+++ inc/template.php
@@ -6,7 +6,7 @@ session_regenerate_id();
 
 
 
-$mysqli = new mysqli("localhost", "root", "", "egon"); //Lägg in våra DB uppgifter
+$mysqli = new mysqli("paracordfashion", "paracordfashion.se.mysql", "	W6AqFUBB", "paracordfashion"); //Lägg in våra DB uppgifter
 $mysqli->set_charset("utf8"); 
 
 ?>
