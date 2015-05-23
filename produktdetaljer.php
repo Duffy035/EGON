07c00f3e0d18b643a0909e91ca6c05b66eb1d74a
Wikberg <snokjonte@hotmail.com>
2015-05-23 16:38:13 +0200

    köpknapp
    köpknapp till paypal ligger i kundvagnsida

--- produktdetaljer.php
+++ produktdetaljer.php
@@ -111,24 +111,6 @@ END;
 			</div>
 		</div>
 
-		<!--<div id="buy">
-			<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="ZH7KU6V93FLBE">
-
-				<input type="hidden" name="item_name" value="<?php echo $name ?>">
-				<input type="hidden" name="item_number" value="<?php echo $productid ?>">
-				<input type="hidden" name="amount" value="<?php echo $price ?>">
-				<input type="hidden" id="colour1" name="item_color1" value="">
-				<input type="hidden" id="colour2" name="item_color2" value="">
-				<input type="hidden" id="size" name="item_stl" value="">
-				<input type="hidden" id="material" name="item_mtrl" value="">
-
-				<input type="hidden" name="currency_code" value="SEK">
-				<input type="image" src="https://www.paypalobjects.com/sv_SE/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal – ett tryggt och smidigt sätt att betala på nätet med.">
-				<img alt="" border="0" src="https://www.paypalobjects.com/sv_SE/i/scr/pixel.gif" width="1" height="1">
-			</form>
-		</div>-->
 	</div>
 </div>
 
