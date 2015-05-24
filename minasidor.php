ef70ba1c9ac2c0b7f99297e72e6fea4b54bbaf41
adamzki <aadammzkii@gmail.com>
2015-05-24 15:58:43 +0200

    Mina sidor
    går nu uppdatera uppgifter

--- minasidor.php
+++ minasidor.php
@@ -3,7 +3,7 @@
 	include('inc/nav.php');
 	$content = '';
 
-	if(isset($_GET['userid'])) {
+	if(isset($_SESSION['userid'])) {
 		if(isset($_POST['password'])) {
 			$query = <<<END
 			UPDATE user
@@ -14,28 +14,33 @@
 			email = '{$_POST['email']}',
 			phone = '{$_POST['phone']}',
 			password = '{$_POST['password']}'
+			WHERE userid = '{$_SESSION['userid']}' AND password = '{$_POST['password']}'
 END;
 			$mysqli->query($query);
 		}
+
 		$query = <<<END
 		SELECT * FROM user
-		WHERE userid = '{$_GET['userid']}'
+		WHERE userid = '{$_SESSION['userid']}'
 END;
 		
 		$res = $mysqli->query($query);
 		if($res->num_rows > 0) {
 			$row = $res->fetch_object();
 			$content .= <<<END
-			<form method="post" action="edit_user.php?userid={$row->userid}">
-				<input type="text" name="fname" value="{$row->fname}"><br>
-				<input type="text" name="lname" value="{$row->lname}"><br>
-				<input type="text" name="street" value="{$row->street}"><br>
-				<input type="text" name="zipcode" value="{$row->zipcode}"><br>
-				<input type="text" name="email" value="{$row->email}"><br>
-				<input type="text" name="phone" value="{$row->phone}"><br>
-				<input type="password" name="password" value="{$row->password}"><br>
-				<input type="submit" value="Spara">
-			</form>
+			<div class="container">
+				<form method="post" action="minasidor.php">
+					<input type="text" name="fname" value="{$row->fname}"><br>
+					<input type="text" name="lname" value="{$row->lname}"><br>
+					<input type="text" name="street" value="{$row->street}"><br>
+					<input type="text" name="zipcode" value="{$row->zipcode}"><br>
+					<input type="text" name="email" value="{$row->email}"><br>
+					<input type="text" name="phone" value="{$row->phone}"><br>
+					<input type="password" name="password" placeholder="Lösenord"><br>
+					<input type="submit" value="Spara">
+				</form>
+				<p>Skriv in lösenord och tryck på Spara för ändra</p>
+			</div>
 END;
 		}
 	}
