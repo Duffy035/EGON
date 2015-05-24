d5396173d634fe91626c8db31e58728cbb6030cb
adamzki <aadammzkii@gmail.com>
2015-05-24 12:17:38 +0200

    template fix

--- inc/template.php
+++ inc/template.php
@@ -1,21 +1,14 @@
 <?php
 session_name('Paracord_webbshop');
 session_start();
-<<<<<<< HEAD
+
 $currency = '$'; //Currency sumbol or code
-=======
-//session_regenerate_id();
->>>>>>> origin/master
+
 //$mysqli = new mysqli("paracordfashion.se.mysql", "paracordfashion", "W6AqFUBB", "paracordfashion"); //Lägg in våra DB uppgifter
 
 $mysqli = new mysqli("localhost", "root", "", "egon"); //Lägg in våra DB uppgifter
 $mysqli->set_charset("utf8");
 
-
-/*if (!isset($_SESSION['cart']))
-{
-	$_SESSION['cart'] = array();
-}*/
 
 //paypal settings
 $PayPalMode 			= 'sandbox'; // sandbox or live
