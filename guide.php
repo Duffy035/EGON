a6223617f81bbc7d9246248c6238e8a0fbefd404
Wikberg <snokjonte@hotmail.com>
2015-05-24 11:18:21 +0200

    responsiva bilder

--- guide.php
+++ guide.php
@@ -6,25 +6,25 @@
 		<div class="tajt">
 			<div class="row">
 				<div class="col-xs-4">
-			    	<img src="img/yellow.jpeg" alt="Gul" class="guideimg">
+			    	<img src="img/yellow.jpeg" alt="Gul" class="img-responsive" class="guideimg">
 			    	<p>Gul färg</p>
 			    </div>
 			    <div class="col-xs-4">
-			    	<img src="img/grey.jpeg" alt="Grå" class="guideimg">
+			    	<img src="img/grey.jpeg" alt="Grå" class="img-responsive" class="guideimg">
 			    	<p>Grå färg</p>
 			    </div>
 			    <div class="col-xs-4">
-			    	<img src="img/red.jpeg" alt="Red" class="guideimg">
+			    	<img src="img/red.jpeg" alt="Red" class="img-responsive" class="guideimg">
 			    	<p>Röd färg</p>
 			    </div>	
 			</div>
 			<div class="row">
 				<div class="col-xs-4">
-					<img src="img/blue.jpeg" alt="Blå" class="guideimg">
+					<img src="img/blue.jpeg" alt="Blå" class="img-responsive" class="guideimg">
 			    	<p>Blå färg</p>
 			   	</div>
 		       	<div class="col-xs-4">
-		       		<img src="img/black.jpeg" alt="Svart" class="guideimg">
+		       		<img src="img/black.jpeg" alt="Svart" class="img-responsive" class="guideimg">
 			   		<p>Svart färg</p>
 				 </div>
 			</div>
