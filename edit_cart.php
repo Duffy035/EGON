4cd0399b02e660bac3d0fc7a47ba792a28a6cf6b
adamzki <aadammzkii@gmail.com>
2015-05-24 11:02:02 +0200

    pill med kundvagn

--- edit_cart.php
+++ edit_cart.php
@@ -3,11 +3,11 @@
 	include('inc/template.php');
 
 //	foreach ($_SESSION['cart'] as $item => $cart_item) {
-	$id = $_GET['row_id'];
+	$id = $_GET['productid'];
 	/*echo $id;
 	$id--;
 	echo $id;*/
-	unset($_SESSION['cart'][$id--]);
+	unset($_SESSION['cart'][$id]);
 //}
 
 header("Location:kundvagn.php")
--- kundvagn.php
+++ kundvagn.php
@@ -11,12 +11,11 @@
 <div class="container">
 	<h3>Här ser du din varukorg!</h3><hr>
 	<?php
-	$row_id = 0;
-	 foreach ($_SESSION['cart'] as $item => $cart_item) { $row_id++; ?>
+	 foreach ($_SESSION['cart'] as $item => $cart_item) { $productid = $cart_item["productid"]; ?>
 		<strong>Namn:</strong> <?php echo $cart_item["name"]; ?>
 		<strong>Á pris:</strong> <?php echo $cart_item['price'] ?> 
 		<strong>Antal:</strong><?php echo $cart_item['qty'] ?>
-		<strong>Total Pris:</strong> <?php echo $cart_item['price'] * $cart_item['qty']; ?> <a href="edit_cart.php?row_id=<?php echo $row_id; ?>">Ta bort</a><hr>
+		<strong>Total Pris:</strong> <?php echo $cart_item['price'] * $cart_item['qty']; ?> <a href="edit_cart.php?productid=<?php echo $productid; ?>">Ta bort</a><?php echo $productid; ?><hr>
 	<?php } ?>
 </div>
 
